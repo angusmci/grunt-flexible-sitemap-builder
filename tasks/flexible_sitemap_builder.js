938d582968d21e68982ba3a2f599e327a5c22549
Angus McIntyre <angus@nomadcode.com>
2020-07-24 11:18:20 -0400

    Don’t index files with priority=0.0

--- tasks/flexible_sitemap_builder.js
+++ tasks/flexible_sitemap_builder.js
@@ -6,165 +6,178 @@
  * Licensed under the MIT license.
  */
 
-'use strict';
+"use strict";
 
-module.exports = function(grunt) {
-
-  var path = require('path');
-  var fs = require('fs');
-  var zlib = require('zlib');
+module.exports = function (grunt) {
+  var path = require("path");
+  var fs = require("fs");
+  var zlib = require("zlib");
   var htmlparser = require("htmlparser2");
 
   // Please see the Grunt documentation for more information regarding task
   // creation: http://gruntjs.com/creating-tasks
 
-  grunt.registerMultiTask('flexible_sitemap_builder', 'A sitemap builder plugin for Grunt', function() {
-    // Merge task-specific and/or target-specific options with these defaults.
-    var options = this.options({
-      baseurl: 'http://example.com/',
-      compress: true,
-      indexes: 'index.html'
-    });
+  grunt.registerMultiTask(
+    "flexible_sitemap_builder",
+    "A sitemap builder plugin for Grunt",
+    function () {
+      // Merge task-specific and/or target-specific options with these defaults.
+      var options = this.options({
+        baseurl: "http://example.com/",
+        compress: true,
+        indexes: "index.html",
+      });
 
-	var parserStore = [];
-	var parser = buildParser(parserStore);
-	var xml = getHeader();
+      var parserStore = [];
+      var parser = buildParser(parserStore);
+      var xml = getHeader();
 
-	// We need to take into account the 'cwd' attribute of the files object,
-	// as users will typically want to change the working directory when
-	// using this task.
-	
-	var cwd_dir = this.files[0].cwd;
-	if (cwd_dir === undefined) {
-		cwd_dir = "";
-	}
+      // We need to take into account the 'cwd' attribute of the files object,
+      // as users will typically want to change the working directory when
+      // using this task.
 
-    // Iterate over all specified file groups.
-    this.files.forEach(function(f) {
-      // Concat specified files.
-      var entry = f.src.filter(function(filepath) {
-        // Warn on and remove invalid source files (if nonull was set).
-        if (!grunt.file.exists(path.join(cwd_dir,filepath))) {
-          grunt.log.warn('Source file "' + filepath + '" not found.');
-          return false;
-        } else {
-          return true;
+      var cwd_dir = this.files[0].cwd;
+      if (cwd_dir === undefined) {
+        cwd_dir = "";
+      }
+
+      // Iterate over all specified file groups.
+      this.files.forEach(function (f) {
+        // Concat specified files.
+        var entry = f.src
+          .filter(function (filepath) {
+            // Warn on and remove invalid source files (if nonull was set).
+            if (!grunt.file.exists(path.join(cwd_dir, filepath))) {
+              grunt.log.warn('Source file "' + filepath + '" not found.');
+              return false;
+            } else {
+              return true;
+            }
+          })
+          .map(function (filepath) {
+            // Read file source.
+            return makeEntry(filepath, cwd_dir, options, parser, parserStore);
+          })
+          .join("");
+
+        xml += entry;
+        xml += getFooter();
+
+        // Compress if required
+
+        if (options.compress) {
+          var compressed = zlib.gzipSync(xml);
+          grunt.file.write(f.dest + ".gz", compressed);
+          grunt.log.writeln('Compressed file "' + f.dest + '.gz" created.');
         }
-      }).map(function(filepath) {
-        // Read file source.
-        return makeEntry(filepath,cwd_dir,options,parser,parserStore);
-      }).join('');
 
-      xml += entry;
-      xml += getFooter();
+        // Otherwise just write out a standard XML file.
+        else {
+          grunt.file.write(f.dest, xml);
+          grunt.log.writeln('File "' + f.dest + '" created.');
+        }
+      });
+    }
+  );
 
-      // Compress if required
-      
-      if (options.compress) {
-        var compressed = zlib.gzipSync(xml);
-        grunt.file.write(f.dest + ".gz",compressed);
-        grunt.log.writeln('Compressed file "' + f.dest + '.gz" created.');
-      }
-      
-      // Otherwise just write out a standard XML file.
-      
-      else {
-        grunt.file.write(f.dest, xml);
-        grunt.log.writeln('File "' + f.dest + '" created.');
-      }
-
-    });
-  });
-  
-  var getHeader = function() {
+  var getHeader = function () {
     return "<?xml version='1.0' encoding='UTF-8'?>\n<urlset xmlns='http://www.sitemaps.org/schemas/sitemap/0.9'>";
   };
-   
-  var getFooter = function() {
-    return '\n</urlset>';
+
+  var getFooter = function () {
+    return "\n</urlset>";
   };
-  
-  var buildParser = function(store) {
-      var parser = new htmlparser.Parser({
-          onopentag: function(name, attribs){
-             if(name === "meta" && attribs.name === "x-sitemap-settings") {
-                 store["sitemap_settings"] = attribs.content;
-             }
-          },
-      });
-      return parser;
+
+  var buildParser = function (store) {
+    var parser = new htmlparser.Parser({
+      onopentag: function (name, attribs) {
+        if (name === "meta" && attribs.name === "x-sitemap-settings") {
+          store["sitemap_settings"] = attribs.content;
+        }
+      },
+    });
+    return parser;
   };
-  
-  var makeEntry = function(filepath,cwd_dir,options,parser,store) {
-    var absfilepath = path.join(cwd_dir,filepath);
+
+  var makeEntry = function (filepath, cwd_dir, options, parser, store) {
+    var absfilepath = path.join(cwd_dir, filepath);
     var stat = fs.lstatSync(absfilepath);
     var timestamp = stat.mtime;
-    var url = build_url(filepath,options);
-    var setting_string = read_setting_string(absfilepath,options,parser,store);
-    if (setting_string === "") {
-        return "";
-    }
-    else {
-        var pieces = setting_string.split(",");
-        var changefreq = pieces[0];
-        var priority = pieces[1];
-        return "\n\t<url>\n\t\t<loc>" + url + "</loc>\n\t\t<changefreq>" + changefreq + "</changefreq>\n\t\t<priority>" + priority + "</priority>\n\t\t<lastmod>" + timestamp.toISOString() + "</lastmod>\n\t</url>";
+    var url = build_url(filepath, options);
+    var setting_string = read_setting_string(
+      absfilepath,
+      options,
+      parser,
+      store
+    );
+    if (setting_string === "" || setting_string.endsWith("0.0")) {
+      return "";
+    } else {
+      var pieces = setting_string.split(",");
+      var changefreq = pieces[0];
+      var priority = pieces[1];
+      return (
+        "\n\t<url>\n\t\t<loc>" +
+        url +
+        "</loc>\n\t\t<changefreq>" +
+        changefreq +
+        "</changefreq>\n\t\t<priority>" +
+        priority +
+        "</priority>\n\t\t<lastmod>" +
+        timestamp.toISOString() +
+        "</lastmod>\n\t</url>"
+      );
     }
   };
-  
-  var build_url = function(filepath,options) {
-    
+
+  var build_url = function (filepath, options) {
     // If the filename is one of the names given in indexes, e.g. 'index.html', then
     // strip off the filename before building the URL.
-    
+
     var filename = path.basename(filepath);
     if (options.indexes.indexOf(filename) >= 0) {
       filepath = path.dirname(filepath);
-      
+
       // Hack the modified filepath slightly to cope with the root index and ensure
       // that subindexes get a trailing slash.
-      
+
       if ("." === filepath) {
         filepath = "";
-      }
-      else {
+      } else {
         filepath = filepath + "/";
       }
     }
-    
+
     // Construct the URL based on the base URL provided in options.
-    
+
     return options.baseurl + filepath;
   };
-  
-  var read_setting_string = function(filepath,options,parser,store) {
-  
-      // Parse the HTML file, looking for an 'x-sitemap-settings' meta element.
-      
-      var html = fs.readFileSync(filepath,'utf8');
-      delete store["sitemap_settings"];
-      parser.write(html);
-      parser.end();
-      
-      // If the file doesn't contain the element, then return a default or nothing.
-      
-      if (store["sitemap_settings"] === undefined) {
-      
-        // Return the default value specified in the options.
-        
-        if (options.default_settings) {
-          return options.default_settings;
-        }
-        
-        // No default value; don't add this file to the map.
-        
-        else {
-          return "";
-        }
+
+  var read_setting_string = function (filepath, options, parser, store) {
+    // Parse the HTML file, looking for an 'x-sitemap-settings' meta element.
+
+    var html = fs.readFileSync(filepath, "utf8");
+    delete store["sitemap_settings"];
+    parser.write(html);
+    parser.end();
+
+    // If the file doesn't contain the element, then return a default or nothing.
+
+    if (store["sitemap_settings"] === undefined) {
+      // Return the default value specified in the options.
+
+      if (options.default_settings) {
+        return options.default_settings;
       }
-      
-      // Return the value found in the file.
-      
-      return store["sitemap_settings"];
+
+      // No default value; don't add this file to the map.
+      else {
+        return "";
+      }
+    }
+
+    // Return the value found in the file.
+
+    return store["sitemap_settings"];
   };
 };
--- test/flexible_sitemap_builder_test.js
+++ test/flexible_sitemap_builder_test.js
@@ -1,6 +1,6 @@
-'use strict';
+"use strict";
 
-var grunt = require('grunt');
+var grunt = require("grunt");
 
 /*
   ======== A Handy Little Nodeunit Reference ========
@@ -23,33 +23,45 @@ var grunt = require('grunt');
 */
 
 exports.flexible_sitemap_builder = {
-  setUp: function(done) {
+  setUp: function (done) {
     // setup here if necessary
     done();
   },
-  default_options: function(test) {
+  default_options: function (test) {
     test.expect(1);
 
-    var actual = grunt.file.read('tmp/default_options.xml.gz');
-    var expected = grunt.file.read('test/expected/default_options.xml.gz');
-    test.equal(actual, expected, 'Default file should be compressed.');
+    var actual = grunt.file.read("tmp/default_options.xml.gz");
+    var expected = grunt.file.read("test/expected/default_options.xml.gz");
+    test.equal(actual, expected, "Default file should be compressed.");
 
     test.done();
   },
-  custom_options: function(test) {
+  custom_options: function (test) {
     test.expect(3);
 
-    var actual1 = grunt.file.read('tmp/custom_options1.xml');
-    var expected1 = grunt.file.read('test/expected/custom_options1.xml');
-    test.equal(actual1, expected1, 'should describe what the custom option(s) behavior is.');
+    var actual1 = grunt.file.read("tmp/custom_options1.xml");
+    var expected1 = grunt.file.read("test/expected/custom_options1.xml");
+    test.equal(
+      actual1,
+      expected1,
+      "File generated using custom options should match expected result file."
+    );
 
-    var actual2 = grunt.file.read('tmp/custom_options2.xml');
-    var expected2 = grunt.file.read('test/expected/custom_options2.xml');
-    test.equal(actual2, expected2, 'should describe what the custom option(s) behavior is.');
-    
-    var actual3 = grunt.file.read('tmp/custom_options3.xml');
-    var expected3 = grunt.file.read('test/expected/custom_options3.xml');
-    test.equal(actual3, expected3, 'should describe what the custom option(s) behavior is.');
+    var actual2 = grunt.file.read("tmp/custom_options2.xml");
+    var expected2 = grunt.file.read("test/expected/custom_options2.xml");
+    test.equal(
+      actual2,
+      expected2,
+      "File generated using custom options should match expected result file."
+    );
+
+    var actual3 = grunt.file.read("tmp/custom_options3.xml");
+    var expected3 = grunt.file.read("test/expected/custom_options3.xml");
+    test.equal(
+      actual3,
+      expected3,
+      "File generated using custom options should match expected result file."
+    );
 
     test.done();
   },
